f754509cc6b53a8433be5c56ccde32c05e923e2f
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-22 17:56:51 +0000

    [Add] make these tables, addresses/items/genres/cart_items/orders/order_details

--- db/migrate/20230422164755_create_items.rb
+++ db/migrate/20230422164755_create_items.rb
@@ -1,7 +1,12 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-
+      
+      t.string :name,       null: false
+      t.text :introduction, null: false
+      t.integer :price,     null: false
+      t.boolean :status,    null: false, default: "TRUE"
+      
       t.timestamps
     end
   end
