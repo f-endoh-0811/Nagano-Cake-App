7ecfe9099d77fa6be5b585f7537fc3aa38576c58
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-23 08:47:34 +0000

    [Add] Make top/about pages of publics, and top page of admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  
+  scope module: :admin do
+    get 'admin' => 'homes#top'
+  end
+  
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about', as: 'about'
+  end
+  
   #管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
