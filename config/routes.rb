3d66f71f18a2edc7425ed1dac902ddc5cabe982d
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-23 12:42:34 +0000

    Add customers sign_up system

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,19 @@ Rails.application.routes.draw do
   end
   
   scope module: :public do
-    root to: 'homes#top'
+    root to: "homes#top"
     get 'about' => 'homes#about', as: 'about'
   end
   
   #管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
+    sessions: 'admin/sessions'
   }
   
   #顧客用
   devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
