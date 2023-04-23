96e353895d29b014806c10657c4226549b9a4fcb
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-23 10:48:06 +0000

    Change is_deleted colum written in migration file 'customers' to 'is_deleted_to_customers'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_22_165115) do
+ActiveRecord::Schema.define(version: 2023_04_23_104041) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -51,9 +51,9 @@ ActiveRecord::Schema.define(version: 2023_04_22_165115) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "telephone_number", null: false
-    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
