d9f44aad7cdf5a585e20413b35930e7d0f64ca9d
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-24 17:34:22 +0000

    [Update] bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,7 +7,7 @@ environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jqueery',
+    jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
   })
 )
