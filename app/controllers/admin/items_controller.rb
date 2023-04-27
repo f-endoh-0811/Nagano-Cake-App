d2729034b739aa955d3bf6de4969e1a9953315c9
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-27 16:45:46 +0000

    Add admin's functions about items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.page(params[:page])
   end
 
   def new
@@ -26,7 +26,7 @@ class Admin::ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
-    redirect_to 
+    redirect_to admin_item_path(item.id)
   end
   
   private
