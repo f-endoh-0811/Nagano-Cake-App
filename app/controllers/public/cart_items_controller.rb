1aa7a0c5ab9cb5190043d650b9f47b4b743f9577
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-05-01 18:24:29 +0000

    [Temporarily saved] Update cart_item's funcion

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,17 +2,20 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
   
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
   
   def create
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.customer_id = current_customer.id
-    cart_item = current_customer.cart_items.find_by(item_id:)
-    if cart_item
-      cart_item += cart_items.amount.to_i
-    else
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:item_id].to_i
       cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_item.save
+      redirect_to cart_items_path
     end
   end
   
@@ -36,7 +39,7 @@ class Public::CartItemsController < ApplicationController
   
   private
   
-  def cart_items_params
+  def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,13 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details
   
-  def get_image(width, height)
-    image.variant(resize_to_fill: [width, height]).processed
+def get_image(width, height)
+  unless image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
+  image.variant(resize_to_limit: [width, height]).processed
+end
   
   # 消費税を求めるメソッド
   def with_tax_price
