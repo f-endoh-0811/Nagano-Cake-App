c270f6446e503f632fe1082d065ea6bea71f1fd0
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-05-05 16:44:24 +0000

    [Add] order functions

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,19 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+  
+  def create
+  end
+  
+  def check
+  end
+  
+  def thanks
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,19 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
   
+  # 管理者用
   namespace :admin do
     resources :genres, only: [:index, :edit, :create, :update]
     resources :items, only: [:index, :new, :show, :edit, :create, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show]
   end
   
   scope module: :admin do
     get 'admin' => 'homes#top', as: 'admin'
   end
   
+  # 顧客用
   scope module: :public do
     root to: "homes#top"
     get 'about' => 'homes#about', as: 'about'
@@ -37,6 +40,12 @@ Rails.application.routes.draw do
     resources :cart_items, only: [:index, :create, :update, :destroy] do
       collection do
         delete 'destroy_all' => 'cart_items#destroy_all'
+      end
+    end
+    resources :orders, only: [:new, :index, :show, :create] do
+      collection do
+        post 'check' => 'orders#check'
+        get 'thanks' => 'orders#thanks'
       end
     end
   end
