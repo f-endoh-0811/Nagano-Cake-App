f0aa6c163445f341739994a4c37d784a072dd56f
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-05-14 14:34:27 +0000

    [Update] Change customer's after sign_in and sign_up path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,27 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!, if: :admin_url
 
- private
-  def after_sign_in_path_for(resource_or_scope)
-    if resource_or_scope.is_a?(Admin)
-      admin_path
-    else
-      root_path
+  private
+    def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(Admin)
+        admin_path
+      else
+        root_path
+      end
     end
 
-  end
-
-  def after_sign_out_path_for(resource_or_scope)
-    if resource_or_scope == :admin
-      new_admin_session_path
-    else
-      root_path
+    def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :admin
+        new_admin_session_path
+      elsif resource_or_scope == :public
+        root_path
+      else
+        root_path
+      end
     end
-  end
 
-  def admin_url
-    request.fullpath.include?("/admin")
-  end
+    def admin_url
+      request.fullpath.include?("/admin")
+    end
 
 end
